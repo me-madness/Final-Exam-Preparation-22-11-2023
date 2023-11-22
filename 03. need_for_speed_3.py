cf27e3e4764c215e57e96a8cf838732548feb00e
me_madness <p_rangel@abv.bg>
2023-11-22 18:00:57 +0100

    def decode_message it's almost done

--- 03. need_for_speed_3.py
+++ 03. need_for_speed_3.py
@@ -1,8 +1,16 @@
 # First task from the lecture
 
 def decode_message(message, commands):
-    
-    
+    for command in commands:
+        tokens = command.split('|')
+        instruction = tokens[0]
+        
+        if instruction == 'Move':
+            num_letters = int(tokens[1])
+            message = message[num_letters:] + message[:num_letters]
+        elif instruction == 'Insert':
+            
+            
 encrypted_message = input()
 commands_list = []
 
