53b9ef3150ff3f92c49b1e91efffaaefafbaebb2
me_madness <p_rangel@abv.bg>
2023-11-22 19:13:05 +0100

    I read the all print

--- 03. need_for_speed_3.py
+++ 03. need_for_speed_3.py
@@ -5,5 +5,14 @@
 
 
 
+
+
+print(f""{car} driven for {distance} kilometers. {fuel} liters of fuel consumed.")
+print("Not enough fuel to make that ride")
+print(f"Time to sell the {car}!")
+print(f"{car} mileage decreased by {amount reverted} kilometers")
+print(f"{car} -> Mileage: {mileage} kms, Fuel in the tank: {fuel} lt.")
+
+
 # Second task is from me
 
