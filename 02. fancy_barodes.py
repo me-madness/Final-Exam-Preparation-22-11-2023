dfa2493bd9a1aba2dfca3d1afd65bde1312bf8c7
me_madness <p_rangel@abv.bg>
2023-11-22 18:34:07 +0100

    02 it's done by the lecture

--- 02. fancy_barodes.py
+++ 02. fancy_barodes.py
@@ -2,9 +2,24 @@
 
 import re
 
-n = int(input())
-barcode = input()
-pattern = r""
+def parse_barcode(barcode_data):
+    pattern = r"@#+[A-Z][A-Za-z0-9]{4,}[A-Z]@#+"
+    
+    
+    for barcode in barcode_data:
+        match = re.fullmatch(pattern, barcode)
+        
+        if match:
+            product_group = ''.join(re.findall(r'\d', barcode))
+            product_group = product_group if product_group else '00'
+            print(F"Product group: {product_group}")
+        else:
+            print(f"Invalid barcode")    
+
+
+num = int(input())
+data = [input() for _ in range(num)]
+parse_barcode(data)
 
 
 
