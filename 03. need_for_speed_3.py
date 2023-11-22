196e9929bb9143309b6468384ba790c4ab3d3c9f
me_madness <p_rangel@abv.bg>
2023-11-22 17:56:01 +0100

    good start pf the lecture

--- 03. need_for_speed_3.py
+++ 03. need_for_speed_3.py
@@ -1,5 +1,20 @@
 # First task from the lecture
 
+def decode_message(message, commands):
+    
+    
+encrypted_message = input()
+commands_list = []
+
+while True:
+    command = input()
+    if command == "Decode":
+        break
+    
+    
+    commands_list.append(command)
+
+decode_message(encrypted_message, commands_list)
 
 
 # Second task is from me
