d9c02c535153121acaba3113486a6ae4d853a930
me_madness <p_rangel@abv.bg>
2023-11-22 18:07:02 +0100

    01 first task is ready

--- 01. the_imitation_game.py
+++ 01. the_imitation_game.py
@@ -1,6 +1,40 @@
 # First task from the lecture
 
+def decode_message(message, commands):
+    for command in commands:
+        tokens = command.split('|')
+        instruction = tokens[0]
+        
+        if instruction == 'Move':
+            num_letters = int(tokens[1])
+            message = message[num_letters:] + message[:num_letters]
+            
+        elif instruction == 'Insert':
+            index = int(tokens[1])
+            value = tokens[2]
+            message = message[:index] + value + message[index:]
+            
+        elif instruction == 'ChangeAll':
+            substring = tokens[1]
+            replacement = tokens[2]
+            message = message.replace(substring, replacement)    
+            
+        elif instruction == 'Decode':
+            print(f"The encrypted message is: {message}")    
+            
+            
+encrypted_message = input()
+commands_list = []
 
+
+while True:
+    command = input()
+    commands_list.append(command)
+    if command == "Decode":
+        break
+   
+    
+decode_message(encrypted_message, commands_list)
 
 # Second task is from me
 
--- 03. need_for_speed_3.py
+++ 03. need_for_speed_3.py
@@ -8,20 +8,32 @@ def decode_message(message, commands):
         if instruction == 'Move':
             num_letters = int(tokens[1])
             message = message[num_letters:] + message[:num_letters]
+            
         elif instruction == 'Insert':
+            index = int(tokens[1])
+            value = tokens[2]
+            message = message[:index] + value + message[index:]
+            
+        elif instruction == 'ChangeAll':
+            substring = tokens[1]
+            replacement = tokens[2]
+            message = message.replace(substring, replacement)    
+            
+        elif instruction == 'Decode':
+            print(f"The encrypted message is: {message}")    
             
             
 encrypted_message = input()
 commands_list = []
 
+
 while True:
     command = input()
+    commands_list.append(command)
     if command == "Decode":
         break
+   
     
-    
-    commands_list.append(command)
-
 decode_message(encrypted_message, commands_list)
 
 
