2ba62e6e01f46886feac36f9ee723d72cba929c4
me_madness <p_rangel@abv.bg>
2023-11-22 19:27:37 +0100

    new commit because

--- 03. need_for_speed_3.py
+++ 03. need_for_speed_3.py
@@ -1,16 +1,52 @@
 # First task from the lecture
 
+def create_car(name, mileage, fuel):
+    return {'name': name, 'mileage': mileage, 'fuel': fuel}
+
+def drive(car, distance, required_fuel):
+    if car['fuel'] >= required_fuel:
+        car['mileage'] += distance
+        car['fuel'] -= required_fuel
+        print(f"{car['name']} driven for {distance} kilometers. {required_fuel} liters of fuel consumed.")
+        
+    else:
+        print("Not enough fuel to make that ride")
+        
+    
+    
+    
+def main_function():
+    n = int(input())
+    cars = []
+    
+    for _ in range(n):
+        car_info = input().split('|')
+        car = create_car(car_info[0], int(car_info[1]), int(car_info[2]))
+        car.append(car)
+   
+    while True:
+        command = input()
+        
+        if command == 'Stop':
+            break
+        
+        tokens = command.split(' : ')
+        action = tokens[0]
+        car_name = tokens[1]
+        
+        
+        for car in cars:
+            if car['name'] == car_name:
+                if action == 'Drive':
+                    distance = int(tokens[2])
+                    fuel = int(tokens[3])
 
 
 
 
 
-
-
-print(f""{car} driven for {distance} kilometers. {fuel} liters of fuel consumed.")
-print("Not enough fuel to make that ride")
 print(f"Time to sell the {car}!")
-print(f"{car} mileage decreased by {amount reverted} kilometers")
+print(f"{car} mileage decreased by {amount_reverted} kilometers")
 print(f"{car} -> Mileage: {mileage} kms, Fuel in the tank: {fuel} lt.")
 
 
